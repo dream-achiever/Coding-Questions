b5b2d9edd78009a2e4465952664d2ad357b77e9d
dream-achiever <rishabhsethia16@gmail.com>
2020-10-11 15:39:47 +0530

    Create Level order traversal Solution

---
+++ GeeksforGeeks/Tree/Levelordertraversal.java
@@ -0,0 +1,60 @@
+/*
+Given a binary tree, find its level order traversal.
+Level order traversal of a tree is breadth-first traversal for the tree.
+
+Example 1:
+
+Input:
+    1
+  /   \ 
+ 3     2
+Output:1 3 2
+
+Example 2:
+
+Input:
+        10
+     /      \
+    20       30
+  /   \
+ 40   60
+Output:10 20 30 40 60 N N
+
+Your Task:
+You don't have to take any input. Complete the function levelOrder() that takes the root node as input parameter and returns a list of integers containing the level order traversal of the given Binary Tree.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+
+Constraints:
+1 <= Number of nodes<= 104
+1 <= Data of a node <= 104
+#################################################################################Solution###########################################################################
+*/
+class Level_order_traversal
+{
+    //You are required to complete this method
+    static ArrayList <Integer> levelOrder(Node node) 
+    {
+        // Your code here
+        ArrayList<Integer> arr = new ArrayList<>();
+        if(node==null){
+            return arr;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
+        while(!q.isEmpty()){
+            Node curr = q.poll();
+            arr.add(curr.data);
+            if(curr.left!=null){
+                q.add(curr.left);
+            }
+            if(curr.right!=null){
+                q.add(curr.right);
+            }
+        }
+        return arr;
+    }
+}
