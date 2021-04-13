ce7382c8aeec017df24ed25305a729205568637c
dream-achiever <rishabhsethia16@gmail.com>
2021-04-13 07:55:32 +0530

    Create K Largest Elements Solution

---
+++ GeeksforGeeks/Heap/KLargestElements.java
@@ -0,0 +1,57 @@
+/*
+Given an array of N positive integers, print k largest elements from the array. 
+
+Example 1:
+
+Input:
+N = 5, k = 2
+arr[] = {12,5,787,1,23}
+Output: 787 23
+Explanation: First largest element in
+the array is 787 and the second largest
+is 23.
+Example 2:
+
+Input:
+N = 7, k = 3
+arr[] = {1,23,12,9,30,2,50}
+Output: 50 30 23
+Explanation: Three Largest element in
+the array are 50, 30 and 23.
+Your Task:
+Complete the function kLargest() that takes the array, N and K as input parameters and returns a list of k largest element in descending order. 
+
+Expected Time Complexity: O(N log K)
+Expected Auxiliary Space: O(K)
+
+Constraints:
+1 ≤ N ≤ 104
+K ≤ N
+1 ≤ array[i] ≤ 105
+
+###############################################################################Solution############################################################################
+*/
+class Solution
+{
+    //Function to return k largest elements from an array.
+    public static ArrayList<Integer> kLargest(int arr[], int n, int k)
+    {
+        // code here 
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0;i<k;i++){
+            pq.add(arr[i]);
+        }
+        for(int i=k;i<n;i++){
+            if(arr[i]>pq.peek()){
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        ArrayList<Integer> a = new ArrayList<>();
+        while(!pq.isEmpty()){
+            a.add(pq.poll());
+        }
+        Collections.reverse(a);
+        return a;
+    }
+}
