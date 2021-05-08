f8148dcf9b4c3900dc2e4af3b651e2a4916dccf9
dream-achiever <rishabhsethia16@gmail.com>
2021-05-08 17:13:09 +0530

    Create DFS of Graph Solution

---
+++ GeeksforGeeks/Graph/DFSOfGraph.java
@@ -0,0 +1,73 @@
+/*
+Given a connected undirected graph. Perform a Depth First Traversal of the graph.
+Note: Use recursive approach to find the DFS traversal of the graph starting from the 0th vertex from left to right according to the graph..
+
+
+Example 1:
+
+Input:
+
+Output: 0 1 2 4 3
+Explanation: 
+0 is connected to 1, 2, 4.
+1 is connected to 0.
+2 is connected to 0.
+3 is connected to 0.
+4 is connected to 0, 3.
+so starting from 0, it will go to 1 then 2
+then 4, and then from 4 to 3.
+Thus dfs will be 0 1 2 4 3.
+Example 2:
+
+Input:
+
+Output: 0 1 2 3
+Explanation:
+0 is connected to 1 , 3.
+1 is connected to 2. 
+2 is connected to 1.
+3 is connected to 0. 
+so starting from 0, it will go to 1 then 2
+then back to 0 then 0 to 3
+thus dfs will be 0 1 2 3. 
+
+Your task:
+You don’t need to read input or print anything. Your task is to complete the function dfsOfGraph() which takes the integer V denoting the number of vertices and adjacency list as input parameters and returns  a list containing the DFS traversal of the graph starting from the 0th vertex from left to right according to the graph.
+
+
+Expected Time Complexity: O(V + E)
+Expected Auxiliary Space: O(V)
+
+
+Constraints:
+1 ≤ V, E ≤ 104
+#######################################################################################Solution#####################################################################
+*/
+class Solution
+{
+    
+    public ArrayList<Integer> dfsRec(int V,ArrayList<ArrayList<Integer>> adj,boolean visited[],ArrayList<Integer> a,int s){
+            a.add(s);
+            visited[s]=true;
+            for(int v : adj.get(s)){
+                if(!visited[v]){
+                    visited[v]=true;
+                    a=dfsRec(V,adj,visited,a,v);
+                }
+            }
+        
+        return a;
+    }
+    
+    //Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+        // Code here
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+        boolean visited[] = new boolean[V];
+        arr = dfsRec(V,adj,visited,arr,0);
+        return arr;
+        
+    }
+}
